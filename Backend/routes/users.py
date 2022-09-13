10ab043222b7e7094eaaad79b67fafb305d2ddfc
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-09-13 12:46:53 +0100

    Feat: User routes creation

---
+++ Backend/routes/users.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, HTTPException, status
+from models.user import User, UserSignIn
+
+user_router = APIRouter(tags["User"], prefix="/user")
+
+""" Create User Authentication """
+
+users = {}
+
+
+@user_router.post("/", response_model=User)
+async def create_user(data: User):
+    if data.email in users:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"User with email {data.email} already exists",
+        )
+    users[data.email] = data
+
+    return {"message": "User successfully registered!!!"}
+
+
+@user_router.post("login")
+async def login_user(user: UserSignIn):
+    if users[user.email] not in users:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User does not exists"
+        )
+    if users[user.email].password != user.password:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Wrong credentials passed"
+        )
+
+    return {"message": "User signed in successfully"}
