1a1f8bd4018ae3cfc3ca00a0632e772a9f2bade4
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-09-14 17:02:05 +0100

    CRUD Operation for Blog

---
+++ Backend/crud/crud_blog.py
@@ -0,0 +1,20 @@
+from crud.base import CRUDBase
+from fastapi.encoders import jsonable_encoder
+from models.blog import Blog
+from schemas.blog import BlogUpdate, BlogCreate
+from sqlalchemy.orm import Session
+
+
+class CRUDItem(CRUDBase[Blog, BlogCreate, BlogUpdate]):
+    def create_with_owner(self, db: Session, *, obj_in: BlogCreate, owner_id: int):
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data, owner_id= owner_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def get_multi_by_owner(self, db: Session, *, owner_id: int, skip: int= 0, limit: int = 100):
+        return (db.query(self.model).first(Blog.owner_id == owner_id).offset(skip).limit(limit).all())
+    
+blog = CRUDItem(Blog)
