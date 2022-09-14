7bb5c7c7e01e347a3634a48159fdcad6b45243fb
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-09-14 14:28:00 +0100

    restructuring database

--- Backend/database/connection.py
+++ Backend/database/connection.py
@@ -5,26 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import sessionmaker
 
+from database.session import SessionLocal
 
 
-
-# Enable this for Postgresql database connection 
-
-# database_connection_string = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-
-# engine = create_engine(database_connection_string, echo=True)
-
-
-# SQLite database connection 
-database_file = "blog.db"
-connect_args = {"check_same_thread": False}
-
-database_connection_string = f"sqlite:///{database_file}"
-engine = create_engine(database_connection_string, echo=True, connect_args=connect_args) # sqlite
-
-
-# Session
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
---
+++ Backend/database/session.py
@@ -0,0 +1,27 @@
+from sqlmodel import SQLModel, Session, create_engine
+from core.config import settings
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import sessionmaker
+
+""" This module contains session connection the database"""
+
+
+# Enable this for Postgresql database connection 
+
+# database_connection_string = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+
+# engine = create_engine(database_connection_string, echo=True)
+
+
+# SQLite database connection 
+database_file = "blog.db"
+connect_args = {"check_same_thread": False}
+
+database_connection_string = f"sqlite:///{database_file}"
+engine = create_engine(database_connection_string, echo=True, connect_args=connect_args) # sqlite
+
+
+# Session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
