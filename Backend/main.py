7dc322144198b6993abfdf94f42bd221f049d9d8
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-09-15 23:28:08 +0100

    upated main.py for cleaner code

--- Backend/main.py
+++ Backend/main.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI
 import uvicorn
 from routes.users import user_router
 from routes.blog import post_router
+from routes.auth import auth_router
 
 app = FastAPIOffline()
 
-app.include_router(user_router)
-app.include_router(post_router)
+app.include_router(auth_router, prefix="/auth")
+app.include_router(user_router, prefix="/user")
+app.include_router(post_router, prefix="/post")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
