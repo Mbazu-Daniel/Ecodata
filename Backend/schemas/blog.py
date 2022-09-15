f6fd9f634bc33bad21f4922f1eb07e7252424c92
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-09-15 23:18:33 +0100

    token and tokenpayload schema

--- Backend/schemas/blog.py
+++ Backend/schemas/blog.py
@@ -1,13 +1,35 @@
 from pydantic import BaseModel
 from typing import Optional, List
 
-class Blog(BaseModel):
-    id: int
+
+# Shared properties
+class BlogBase(BaseModel):
+    title: Optional[str] = None
+    body: Optional[str] = None
+    image: Optional[str] = None
+    published: bool = True
+
+# Blog creation schema
+class BlogCreate(BlogBase):
     title: str
-    image: str
     body: str
-    tags: List[str]
-    author: Optional[str]
+    image: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Introduction to Economics",
+                "image": "https://linktomyimage.com/image.png",
+                "body": "Economics is the study of the nations wealth",
+                "published": True
+            }
+        }
+
+    #
+
+
+class BlogUpdate(BlogBase):
+    pass
 
     class Config:
         schema_extra = {
@@ -15,6 +37,28 @@ class Blog(BaseModel):
                 "title": "Introduction to Economics",
                 "image": "https://linktomyimage.com/image.png",
                 "tags": ["Economics", "Micro Economics", "Macro Economics"],
-                "body": "Economics is the study of the nations wealth"
+                "body": "Economics is the study of the nations wealth",
+                "published": True
             }
         }
+
+
+""" Blog data stored in the database"""
+
+# Properties shared by models stored in DB
+class BlogInDBBase(BlogBase):
+    id: int
+    title: str
+    owner_id: int 
+    
+    class Config: 
+        orm_mode = True
+        
+# Properties stored in DB
+class BlogInDB(BlogInDBBase):
+    pass
+
+# Properties to return to client
+class BlogSchema(BlogInDBBase):
+    pass
+        
--- Backend/schemas/users.py
+++ Backend/schemas/users.py
@@ -1,28 +1,71 @@
+from datetime import datetime
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
-class User(BaseModel):
-    full_name: str
-    email: str
+date = datetime.now()
+
+
+# Shared Properties for Users
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
     password: str
+    is_active: Optional[bool] = True
+    is_superuser: bool = False
+    created_at: datetime
 
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    pass
 
     class Config:
         schema_extra = {
             "example": {
-                "full_name": "John Doe",
+                "first_name": "John",
+                "last_name": "Doe",
                 "email": "djangodaniel97@gmail.com",
-                "password": "Password#123"
+                "password": "Password#123",
+                "is_active": True,
+                "is_superuser": False,
+                "created_at": date
             }
         }
 
-class UserSignIn(BaseModel):
+
+# Properties to receive via API on Login
+class UserSignInSchema(BaseModel):
     email: EmailStr
     password: str
 
     class Config:
         schema_extra = {
-            "example": {
-                "email": "djangodaniel97@gmail.com",
-                "password": "Password#123"
-            }
+            "example": {"email": "djangodaniel97@gmail.com", "password": "Password#123"}
         }
+
+
+""" You need to understand this Boy"""
+
+
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserInDBBase(UserBase):
+    id: Optional[int] = None
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+# Additional properties to return via API
+class UserOut(UserInDBBase):
+    pass
+
+# # Additional properties stored in DB
+# class UserInDB(UserInDBBase):
+#     hashed_password: str
